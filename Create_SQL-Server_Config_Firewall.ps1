bd45d25bf84f785c577f7b3f5a0975f243b70065
Tom Wechsler <56835948+tomwechsler@users.noreply.github.com>
2020-04-18 15:28:38 +0200

    Add files via upload

---
+++ Create_SQL-Server_Config_Firewall.ps1
@@ -0,0 +1,54 @@
+﻿Set-Location c:\
+Clear-Host
+
+Install-Module -Name Az -Force -AllowClobber -Verbose
+
+#Log into Azure
+Connect-AzAccount
+
+#Select the correct subscription
+Get-AzSubscription -SubscriptionName "MSDN Platforms" | Select-AzSubscription
+
+# Set the resource group name and location for your server
+$resourceGroupName = "myResourceGroup-$(Get-Random)"
+$location = "westeurope"
+
+# Set an admin login and password for your server
+$adminSqlLogin = "SqlAdmin"
+$password = "P@ssw0rd123!"
+
+# Set server name - the logical server name has to be unique in the system
+$serverName = "server-$(Get-Random)"
+
+# The sample database name
+$databaseName = "mySampleDatabase"
+
+# The ip address range that you want to allow to access your server
+$startIp = "188.61.132.132"
+$endIp = "188.61.132.132"
+
+# Create a resource group
+$resourceGroup = New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+# Create a server with a system wide unique server name
+$server = New-AzSqlServer -ResourceGroupName $resourceGroupName `
+    -ServerName $serverName `
+    -Location $location `
+    -SqlAdministratorCredentials $(New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adminSqlLogin, $(ConvertTo-SecureString -String $password -AsPlainText -Force))
+
+# Create a server firewall rule that allows access from the specified IP range
+$serverFirewallRule = New-AzSqlServerFirewallRule -ResourceGroupName $resourceGroupName `
+    -ServerName $serverName `
+    -FirewallRuleName "AllowedIPs" -StartIpAddress $startIp -EndIpAddress $endIp
+
+# Create a blank database with an S0 performance level
+$database = New-AzSqlDatabase  -ResourceGroupName $resourceGroupName `
+    -ServerName $serverName `
+    -DatabaseName $databaseName `
+    -RequestedServiceObjectiveName "S0" `
+    -SampleName "AdventureWorksLT"
+
+#Check access with the SQL Server Management Studio
+
+# Clean up deployment 
+Remove-AzResourceGroup -ResourceGroupName $resourceGroupName
