7852a10533300e39e052d265207814bb7f28f7b7
Tom Wechsler <56835948+tomwechsler@users.noreply.github.com>
2020-04-27 16:34:48 +0200

    Add files via upload

---
+++ Create_Azure_Firewall.ps1
@@ -0,0 +1,130 @@
+﻿Set-Location c:\
+Clear-Host
+
+Install-Module -Name Az -Force -AllowClobber -Verbose
+
+#Log into Azure
+Connect-AzAccount
+
+#Select the correct subscription
+Get-AzSubscription -SubscriptionName "MSDN Platforms" | Select-AzSubscription
+
+#Create a resource group
+New-AzResourceGroup -Name Test-FW-RG -Location "WestEurope"
+
+#Create three subnets
+$FWsub = New-AzVirtualNetworkSubnetConfig -Name AzureFirewallSubnet -AddressPrefix 10.0.1.0/26
+$Worksub = New-AzVirtualNetworkSubnetConfig -Name Workload-SN -AddressPrefix 10.0.2.0/24
+$Jumpsub = New-AzVirtualNetworkSubnetConfig -Name Jump-SN -AddressPrefix 10.0.3.0/24
+
+#Create a VNet
+$testVnet = New-AzVirtualNetwork -Name Test-FW-VN -ResourceGroupName Test-FW-RG `
+-Location "WestEurope" -AddressPrefix 10.0.0.0/16 -Subnet $FWsub, $Worksub, $Jumpsub
+
+#Create the Srv-Jump virtual machine
+New-AzVm `
+    -ResourceGroupName Test-FW-RG `
+    -Name "Srv-Jump" `
+    -Location "WestEurope" `
+    -VirtualNetworkName Test-FW-VN `
+    -SubnetName Jump-SN `
+    -OpenPorts 3389 `
+    -Size "Standard_D2s_v3"
+
+#Create a workload virtual machine with no public IP address
+#Create the NIC
+$NIC = New-AzNetworkInterface -Name Srv-work -ResourceGroupName Test-FW-RG `
+ -Location "WestEurope" -Subnetid $testVnet.Subnets[1].Id 
+
+#Define the virtual machine
+$VirtualMachine = New-AzVMConfig -VMName Srv-Work -VMSize "Standard_D2s_v3"
+$VirtualMachine = Set-AzVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName Srv-Work -ProvisionVMAgent -EnableAutoUpdate
+$VirtualMachine = Add-AzVMNetworkInterface -VM $VirtualMachine -Id $NIC.Id
+$VirtualMachine = Set-AzVMSourceImage -VM $VirtualMachine -PublisherName 'MicrosoftWindowsServer' -Offer 'WindowsServer' -Skus '2016-Datacenter' -Version latest
+
+#Create the virtual machine
+New-AzVM -ResourceGroupName Test-FW-RG -Location "WestEurope" -VM $VirtualMachine -Verbose
+
+#Deploy the firewall
+# Get a Public IP for the firewall
+$FWpip = New-AzPublicIpAddress -Name "fw-pip" -ResourceGroupName Test-FW-RG `
+  -Location "WestEurope" -AllocationMethod Static -Sku Standard
+# Create the firewall
+$Azfw = New-AzFirewall -Name Test-FW01 -ResourceGroupName Test-FW-RG -Location "WestEurope" -VirtualNetworkName Test-FW-VN -PublicIpName fw-pip
+
+#Save the firewall private IP address for future use
+$AzfwPrivateIP = $Azfw.IpConfigurations.privateipaddress
+$AzfwPrivateIP
+
+#Create a default route, with BGP route propagation disabled
+
+$routeTableDG = New-AzRouteTable `
+  -Name Firewall-rt-table `
+  -ResourceGroupName Test-FW-RG `
+  -location "WestEurope" `
+  -DisableBgpRoutePropagation
+
+#Create a route
+ Add-AzRouteConfig `
+  -Name "DG-Route" `
+  -RouteTable $routeTableDG `
+  -AddressPrefix 0.0.0.0/0 `
+  -NextHopType "VirtualAppliance" `
+  -NextHopIpAddress $AzfwPrivateIP `
+ | Set-AzRouteTable
+
+#Associate the route table to the subnet
+Set-AzVirtualNetworkSubnetConfig `
+  -VirtualNetwork $testVnet `
+  -Name Workload-SN `
+  -AddressPrefix 10.0.2.0/24 `
+  -RouteTable $routeTableDG | Set-AzVirtualNetwork
+
+#Configure an application rule, allows outbound access to www.google.com
+$AppRule1 = New-AzFirewallApplicationRule -Name Allow-Google -SourceAddress 10.0.2.0/24 `
+  -Protocol http, https -TargetFqdn www.google.com
+
+$AppRuleCollection = New-AzFirewallApplicationRuleCollection -Name App-Coll01 `
+  -Priority 200 -ActionType Allow -Rule $AppRule1
+
+$Azfw.ApplicationRuleCollections = $AppRuleCollection
+
+Set-AzFirewall -AzureFirewall $Azfw 
+ 
+#Configure a network rule, allows outbound access to two IP addresses at port 53
+$NetRule1 = New-AzFirewallNetworkRule -Name "Allow-DNS" -Protocol UDP -SourceAddress 10.0.2.0/24 `
+   -DestinationAddress 209.244.0.3,209.244.0.4 -DestinationPort 53
+
+$NetRuleCollection = New-AzFirewallNetworkRuleCollection -Name RCNet01 -Priority 200 `
+   -Rule $NetRule1 -ActionType "Allow"
+
+$Azfw.NetworkRuleCollections = $NetRuleCollection
+
+Set-AzFirewall -AzureFirewall $Azfw
+
+#Change the primary and secondary DNS address for the Srv-Work network interface. This isn't a general Azure Firewall requirement.
+$NIC.DnsSettings.DnsServers.Add("209.244.0.3")
+$NIC.DnsSettings.DnsServers.Add("209.244.0.4")
+$NIC | Set-AzNetworkInterface
+
+#Test the firewall
+#Note the private IP address for the Srv-Work virtual machine
+$NIC.IpConfigurations.PrivateIpAddress
+
+#Connect a remote desktop to Srv-Jump virtual machine, and sign in. 
+#From there, open a remote desktop connection to the Srv-Work private IP address and sign in
+#On SRV-Work, open a PowerShell window and run the following commands:
+nslookup www.google.com
+nslookup www.microsoft.com
+
+#Run the following commands:
+Invoke-WebRequest -Uri https://www.google.com
+Invoke-WebRequest -Uri https://www.google.com
+
+#The www.google.com requests should succeed, and the www.microsoft.com requests should fail.
+Invoke-WebRequest -Uri https://www.microsoft.com
+Invoke-WebRequest -Uri https://www.microsoft.com
+
+
+#Clean up resources
+Remove-AzResourceGroup -Name Test-FW-RG  
