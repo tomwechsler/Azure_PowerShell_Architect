bd5ffed321ffc28ce9322e34beff826f6b8bc856
Tom Wechsler <56835948+tomwechsler@users.noreply.github.com>
2020-04-30 16:15:28 +0200

    Add files via upload

---
+++ Rotate_storage_account_access_keys.ps1
@@ -0,0 +1,48 @@
+﻿Set-Location c:\
+Clear-Host
+
+Install-Module -Name Az -Force -AllowClobber -Verbose
+
+#Prefix for resources
+$prefix = "tw"
+
+#Basic variables
+$id = Get-Random -Minimum 1000 -Maximum 9999
+
+#Log into Azure
+Connect-AzAccount
+
+#get list of locations and pick one
+Get-AzLocation | select Location
+$location = "westeurope"
+
+#Select the correct subscription
+Get-AzSubscription -SubscriptionName "MSDN Platforms" | Select-AzSubscription
+
+#create a resource group
+$resourceGroup = "$prefix-rg-$id"
+New-AzResourceGroup -Name $resourceGroup -Location $location 
+
+#create a standard general-purpose storage account 
+$storageAccountName = "$($prefix)sa$id"
+New-AzStorageAccount -ResourceGroupName $resourceGroup `
+  -Name $storageAccountName `
+  -Location $location `
+  -SkuName Standard_LRS `
+
+#retrieve the first storage account key and display it 
+$storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroup -Name $storageAccountName).Value[0]
+
+Write-Host "storage account key 1 = " $storageAccountKey
+
+#re-generate the key
+New-AzStorageAccountKey -ResourceGroupName $resourceGroup `
+    -Name $storageAccountName `
+    -KeyName key1
+
+#retrieve it again and display it 
+$storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroup -Name $storageAccountName).Value[0]
+Write-Host "storage account key 1 = " $storageAccountKey
+
+#Clean Up
+Remove-AzResourceGroup -Name $resourceGroup -Force
