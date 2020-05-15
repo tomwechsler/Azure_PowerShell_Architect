980eb9c61bdc6b5dd40ffd9c0bf589ab8f5ef66c
Tom Wechsler <56835948+tomwechsler@users.noreply.github.com>
2020-05-15 13:51:06 +0200

    Add files via upload

---
+++ Connect_App_Service_app_SQL_database.ps1
@@ -0,0 +1,50 @@
+﻿Set-Location c:\
+Clear-Host
+
+Install-Module -Name Az -Force -AllowClobber -Verbose
+
+#Log into Azure
+Connect-AzAccount
+
+#Select the correct subscription
+Get-AzSubscription -SubscriptionName "MSDN Platforms" | Select-AzSubscription
+
+# Generates a Random Value
+$Random=(New-Guid).ToString().Substring(0,8)
+
+# Variables
+$ResourceGroup="MyResourceGroup$Random"
+$AppName="webappwithSQL$Random"
+$Location="WestEurope"
+$ServerName="webappwithsql$Random"
+$StartIP="0.0.0.0"
+$EndIP="0.0.0.0"
+$Username="ServerAdmin"
+$Password="P@ssw0rd123!"
+$SqlServerPassword=New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username,(ConvertTo-SecureString -String $Password -AsPlainText -Force)
+
+# Create a Resource Group
+New-AzResourceGroup -Name $ResourceGroup -Location $Location
+
+# Create an App Service Plan
+New-AzAppservicePlan -Name WebAppwithSQLPlan -ResourceGroupName $ResourceGroup -Location $Location -Tier Basic
+
+# Create a Web App in the App Service Plan
+New-AzWebApp -Name $AppName -ResourceGroupName $ResourceGroup -Location $Location -AppServicePlan WebAppwithSQLPlan
+
+# Create a SQL Database Server
+New-AzSQLServer -ServerName $ServerName -Location $Location -SqlAdministratorCredentials $SqlServerPassword -ResourceGroupName $ResourceGroup
+
+# Create Firewall Rule for SQL Database Server
+New-AzSqlServerFirewallRule -FirewallRuleName "AllowYourIp" -StartIpAddress $StartIP -EndIPAddress $EndIP -ServerName $ServerName -ResourceGroupName $ResourceGroup
+
+# Create SQL Database in SQL Database Server
+New-AzSQLDatabase -ServerName $ServerName -DatabaseName MySampleDatabase -ResourceGroupName $ResourceGroup
+
+# Assign Connection String to Connection String 
+Set-AzWebApp -ConnectionStrings @{ MyConnectionString = @{ Type="SQLAzure"; Value ="Server=tcp:$ServerName.database.windows.net;Database=MySampleDatabase;User ID=$Username@$ServerName;Password=$password;Trusted_Connection=False;Encrypt=True;" } } -Name $AppName -ResourceGroupName $ResourceGroup
+
+#Check the result in the portal
+
+#Clean Up
+Remove-AzResourceGroup -Name myResourceGroup -Force
