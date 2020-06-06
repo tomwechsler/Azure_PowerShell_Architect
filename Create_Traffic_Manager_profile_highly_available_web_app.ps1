97f04af652a49c9da27426b7ef36651e9e0af5aa
Tom Wechsler <56835948+tomwechsler@users.noreply.github.com>
2020-06-06 15:04:55 +0200

    Add files via upload

---
+++ Create_Traffic_Manager_profile_highly_available_web_app.ps1
@@ -0,0 +1,80 @@
+﻿Set-Location c:\
+Clear-Host
+
+Install-Module -Name Az -Force -AllowClobber -Verbose
+
+# Variables
+$Location1="WestEurope"
+
+#Log into Azure
+Connect-AzAccount
+
+#Select the correct subscription
+Get-AzSubscription -SubscriptionName "MSDN Platforms" | Select-AzSubscription
+
+# Create a Resource Group
+New-AzResourceGroup -Name MyResourceGroup -Location $Location1
+
+# Generates a random value
+$Random=(New-Guid).ToString().Substring(0,8)
+$mytrafficmanagerprofile="mytrafficmanagerprofile$Random"
+
+#Create a Traffic Manager profile
+New-AzTrafficManagerProfile `
+-Name $mytrafficmanagerprofile `
+-ResourceGroupName MyResourceGroup `
+-TrafficRoutingMethod Priority `
+-MonitorPath '/' `
+-MonitorProtocol "HTTP" `
+-RelativeDnsName $mytrafficmanagerprofile `
+-Ttl 30 `
+-MonitorPort 80
+
+#Create Web Apps
+
+# Variables
+$App1Name="AppServiceTM1$Random"
+$App2Name="AppServiceTM2$Random"
+$Location1="WestEurope"
+$Location2="EastUS"
+
+# Create an App service plan
+New-AzAppservicePlan -Name "$App1Name-Plan" -ResourceGroupName MyResourceGroup -Location $Location1 -Tier Standard
+New-AzAppservicePlan -Name "$App2Name-Plan" -ResourceGroupName MyResourceGroup -Location $Location2 -Tier Standard
+
+$App1ResourceId=(New-AzWebApp -Name $App1Name -ResourceGroupName MyResourceGroup -Location $Location1 -AppServicePlan "$App1Name-Plan").Id
+$App2ResourceId=(New-AzWebApp -Name $App2Name -ResourceGroupName MyResourceGroup -Location $Location2 -AppServicePlan "$App2Name-Plan").Id
+
+#Add Traffic Manager endpoints
+New-AzTrafficManagerEndpoint -Name "$App1Name-$Location1" `
+-ResourceGroupName MyResourceGroup `
+-ProfileName "$mytrafficmanagerprofile" `
+-Type AzureEndpoints `
+-TargetResourceId $App1ResourceId `
+-EndpointStatus "Enabled"
+
+New-AzTrafficManagerEndpoint -Name "$App2Name-$Location2" `
+-ResourceGroupName MyResourceGroup `
+-ProfileName "$mytrafficmanagerprofile" `
+-Type AzureEndpoints `
+-TargetResourceId $App2ResourceId `
+-EndpointStatus "Enabled"
+
+#Test Traffic Manager profile
+Get-AzTrafficManagerProfile -Name $mytrafficmanagerprofile `
+-ResourceGroupName MyResourceGroup
+
+#Copy the RelativeDnsName value. The DNS name of your Traffic Manager profile is http://<relativednsname>.trafficmanager.net.
+#Open a web browser
+
+#disable your primary site
+Disable-AzTrafficManagerEndpoint -Name $App1Name-$Location1 `
+ -Type AzureEndpoints `
+ -ProfileName $mytrafficmanagerprofile `
+ -ResourceGroupName MyResourceGroup `
+ -Force
+
+#Open a web browser again
+
+#Clean Up
+Remove-AzResourceGroup -Name MyResourceGroup -Force
