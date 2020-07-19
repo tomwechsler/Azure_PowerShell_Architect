1d93cc633d1632c344ba92d395ef57f336b826fd
Tom Wechsler <56835948+tomwechsler@users.noreply.github.com>
2020-07-19 16:35:34 +0200

    Add files via upload

---
+++ Extend_the_ISE_Add_On.ps1
@@ -0,0 +1,3 @@
+﻿#Connect-AzAccount under Add-Ons => Tip from Patrick Gruenauer "PowerShell MVP"
+
+$psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Add("Connect to Azure",{Connect-AzAccount},"CTRL+ALT+A")
